df1626ffd9a704745178ba8677d343030b7824a5
AzureXX <ismat.valiyev@gmail.com>
2018-08-28 22:47:50 +0400

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://ismatv:ismatv1991@ds113442.mlab.com:13442/devconnector",
-  secretOrKey: "secret"
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
